73222c742c227024ed5a481073147fd6497afb47
Gokenve <gokenve@gmail.com>
2023-03-13 22:04:37 +0100

    feat demo's form implementation

--- src/Controller/MaleteoController.php
+++ src/Controller/MaleteoController.php
@@ -2,13 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Demo;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MaleteoController extends AbstractController {
 
-    #[Route('/maleteo')]
-    public function home() {
-        return $this-> render('/maleteo/mainMaleteo.html.twig');
+    #[Route('/maleteo', name: 'maleteo', methods:['GET', 'POST'])]
+    public function home(Request $request, EntityManagerInterface $doctrine) {
+
+        $nombre = $request->request->get('name');
+        $email = $request->request->get('email');
+        $ciudad = $request->request->get('city');
+        $checkbox = $request->request->get('checkbox');
+
+        if ($checkbox === 'on') {
+            $demo = new Demo;
+            $demo -> setName("$nombre");
+            $demo -> setEmail("$email");
+            $demo -> setCity("$ciudad");
+
+            $doctrine->persist($demo);
+
+            $doctrine->flush();
+
+            return $this-> render('/maleteo/mainMaleteo.html.twig', ['demo' => $demo, 'message' => 'Solicitud enviada.']);
+
+        }
+
+        return $this-> render('/maleteo/mainMaleteo.html.twig', ['message' => '']);
+
+
+        
     }
 }  
